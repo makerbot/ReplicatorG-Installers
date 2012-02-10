2888056f0731f70f9323ffa7621db713a6966da0
Ted <Ted@Makerbot.com>
2012-02-10 10:23:28 -0500

    Fixed the VID# in the inf files so windows will now recognize the machines. Some changes to where the drivers are.

--- windows/drivers/install-drivers.bat
+++ windows/drivers/install-drivers.bat
@@ -1,9 +1,14 @@
 mkdir %TEMP%\drivers
 copy /y dpinst.exe %TEMP%\drivers
+copy /y devcon.exe %TEMP%\drivers
 cd "FTDI USB Drivers\"
 copy /y * %TEMP%\drivers
 cd "../Arduino Mega 2560 usbser Driver"
 copy /y * %TEMP%\drivers
+cd "../Makerbot"
+copy /y * %TEMP%\drivers
 cd %TEMP%\drivers
-dpinst.exe /lm
+devcon.exe dp_add MightyBoard.inf
+devcon.exe dp_add Replicator.inf
+devcon.exe rescan
 ping -n 10 127.0.0.1 > nul
