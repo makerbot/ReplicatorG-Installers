1526615ea1b04bac542a4cadf52f473d538b3395
Ted <Ted@Makerbot.com>
2012-02-14 15:22:58 -0500

    First shot at getting support for 32bit driver installation.

--- windows/drivers/install-drivers.bat
+++ windows/drivers/install-drivers.bat
@@ -1,6 +1,11 @@
 mkdir %TEMP%\drivers
-copy /y dpinst.exe %TEMP%\drivers
 copy /y devcon.exe %TEMP%\drivers
+if %PROCESSOR_ARCHITECTURE% == x86 goto :x86
+copy /y dpinst64.exe %TEMP%\drivers\dpinst.exe
+goto :enddpinst
+:x86
+copy /y dpinst86.exe %TEMP%\drivers\dpinst.exe
+:enddpinst
 cd "FTDI USB Drivers\"
 copy /y * %TEMP%\drivers
 cd "../Arduino Mega 2560 usbser Driver"
@@ -8,6 +13,9 @@ copy /y * %TEMP%\drivers
 cd "../Makerbot"
 copy /y * %TEMP%\drivers
 cd %TEMP%\drivers
+
 dpinst.exe /lm
 devcon.exe rescan
+
+
 ping -n 30 127.0.0.1 > nul
